1c804cfbe83b79e0034ce904d5a6e92a3c47bb15
Guilherme Goes Mendonca <guilherme@Guilhermes-MacBook-Pro.local>
2020-03-22 17:11:04 -0300

    feat(Monitor): Style table for better UI/UX

--- src/components/covid-table/index.jsx
+++ src/components/covid-table/index.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
+import classnames from 'classnames'
 import {
   TableContainer,
   Table,
@@ -9,11 +10,14 @@ import {
   TableBody,
   TableRow,
   Paper,
-  CircularProgress,
+  TableSortLabel,
+  Typography,
+  Tooltip,
 } from '@material-ui/core'
 
 import { countriesCasesSelector, loadingCountriesCases } from '_modules/covid/selector'
 import { getCountries, getJHUCovidCases } from '_modules/covid/actions'
+import Loader from '_components/loader'
 
 import columns from './constants'
 import useStyles from './styles'
@@ -21,7 +25,9 @@ import useStyles from './styles'
 const CovidTable = () => {
   const styles = useStyles()
   const dispatch = useDispatch()
-  const { t } = useTranslation(['common'])
+  const { t } = useTranslation(['common', 'country'])
+  const [orderBy, setOrderBy] = useState('cases')
+  const [order, setOrder] = useState('desc')
   const loadingCovidByCountries = useSelector(loadingCountriesCases)
   const covidByCountries = useSelector(countriesCasesSelector)
   const COLUMNS = columns(t)
@@ -31,27 +37,93 @@ const CovidTable = () => {
     dispatch(getJHUCovidCases())
   }, [dispatch])
 
+  const descendingComparator = useCallback(
+    (a, b) => {
+      if (b[orderBy] < a[orderBy]) {
+        return -1
+      }
+      if (b[orderBy] > a[orderBy]) {
+        return 1
+      }
+      return 0
+    },
+    [orderBy]
+  )
+
+  const getComparator = useCallback(() => {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b)
+      : (a, b) => -descendingComparator(a, b)
+  }, [descendingComparator, order])
+
+  const stableSort = useCallback((array, comparator) => {
+    const stabilizedThis = array.map((el, index) => [el, index])
+    stabilizedThis.sort((a, b) => {
+      const orderList = comparator(a[0], b[0])
+      if (orderList !== 0) return orderList
+      return a[1] - b[1]
+    })
+    return stabilizedThis.map(el => el[0])
+  }, [])
+
+  const createSortHandler = useCallback(
+    property => () => {
+      const isDesc = orderBy === property && order === 'desc'
+      setOrder(isDesc ? 'asc' : 'desc')
+      setOrderBy(property)
+    },
+    [order, orderBy]
+  )
+
   if (loadingCovidByCountries) {
-    return <CircularProgress />
+    return <Loader />
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={styles.table} aria-label="simple table">
+    <TableContainer component={Paper} className={styles.container}>
+      <Table className={styles.table} stickyHeader aria-label="sticky table">
         <TableHead>
           <TableRow>
-            {COLUMNS.map(({ format, ...column }) => (
-              <TableCell {...column}>{column.label}</TableCell>
+            {COLUMNS.map(({ format, applyStyle, tooltip, ...headCell }) => (
+              <TableCell
+                {...headCell}
+                padding={headCell.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === headCell.id ? order : false}
+                className={styles[headCell.id]}
+              >
+                <Tooltip title={<Typography>{tooltip}</Typography>}>
+                  <TableSortLabel
+                    active={orderBy === headCell.id}
+                    direction={orderBy === headCell.id ? order : 'asc'}
+                    onClick={createSortHandler(headCell.id)}
+                    className={styles.headCell}
+                  >
+                    <Typography variant="body2"> {headCell.label}</Typography>
+                    {orderBy === headCell.id ? (
+                      <span className={styles.visuallyHidden}>
+                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                      </span>
+                    ) : null}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
             ))}
           </TableRow>
         </TableHead>
         <TableBody>
           {covidByCountries?.length &&
-            covidByCountries?.map(row => (
-              <TableRow key={row.country}>
-                {COLUMNS.map(({ format, ...column }) => (
-                  <TableCell {...column} className={styles.bodyCell}>
-                    {format(row)}
+            stableSort(covidByCountries, getComparator()).map((row, rowIndex) => (
+              <TableRow key={row.country} hover>
+                {COLUMNS.map(({ format, applyStyle, className, ...column }) => (
+                  <TableCell
+                    {...column}
+                    className={classnames(styles[column.id], {
+                      [styles[className(row)]]: applyStyle(row),
+                    })}
+                  >
+                    <Typography variant="body2">
+                      {format({ ...row, place: rowIndex + 1 })}
+                    </Typography>
                   </TableCell>
                 ))}
               </TableRow>
--- src/components/covid-table/styles.js
+++ src/components/covid-table/styles.js
@@ -1,3 +1,71 @@
 import { makeStyles } from '@material-ui/styles'
 
-export default makeStyles(() => ({}))
+export default makeStyles(theme => ({
+  container: {
+    maxHeight: '85vh',
+  },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+  headCell: {
+    textAlign: 'center',
+  },
+  todayCasesRowNone: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.lightGreen,
+  },
+  todayCasesRowLow: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.lightYellow,
+  },
+  todayCasesRowMedium: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.mediumOrange,
+  },
+  todayCasesRowHigh: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.highOrange,
+    '&:MuiTypography-root': {
+      fontWeight: 600,
+    },
+  },
+  todayDeathsRowNone: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.lightGreen,
+  },
+  todayDeathsRowLow: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.lightRed,
+  },
+  todayDeathsRowMedium: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.mediumRed,
+  },
+  todayDeathsRowHigh: {
+    borderRadius: '4px',
+    backgroundColor: theme.palette.custom.highRed,
+    '&:MuiTypography-root': {
+      fontWeight: 600,
+    },
+  },
+  deaths: {
+    color: theme.palette.custom.red,
+  },
+  newDeaths: {
+    color: theme.palette.custom.red,
+  },
+  recovered: {
+    color: theme.palette.custom.green,
+  },
+  critical: {
+    color: theme.palette.custom.safetyOrange,
+  },
+}))
