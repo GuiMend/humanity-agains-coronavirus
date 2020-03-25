40e29448868c57a8ddb3cf67a1884df0c8d7151a
Guilherme Goes Mendonca <guilherme@Guilhermes-MacBook-Pro.local>
2020-03-24 21:42:38 -0300

    feat(Dashboard): Update dashboard layouts and integrate

--- src/views/dashboard/brazil-state/index.jsx
+++ src/views/dashboard/brazil-state/index.jsx
@@ -7,34 +7,33 @@ import { Link as RouterLink } from '@reach/router'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 
 import {
-  allBrazilCasesSelector,
-  loadingBrazilAllCases,
+  brazilCitiesCasesSelector,
+  loadingBrazilCitiesCases,
   lastUpdatedSelector,
 } from '_modules/covid-brazil/selector'
 import { BRAZIL_CITY_DATA_FORMAT } from '_components/covid-table/constants'
-import { getAllBrazilCases, getBrazilLatUpdated } from '_modules/covid-brazil/actions'
+import { getBrazilCitiesCases, getBrazilLatUpdated } from '_modules/covid-brazil/actions'
 import useDashboardStyles from '_views/dashboard/styles'
 import { formatTimeZoneDate } from '_utils/date-format'
 import CasesSummary from '_components/cases-summary'
 import CovidTable from '_components/covid-table'
 
+import useStyles from './styles'
+
 const BrazilStateDashboard = ({ state }) => {
+  const styles = useStyles()
   const stylesDashboard = useDashboardStyles()
   const { t } = useTranslation()
   const dispatch = useDispatch()
-  const allBrazilCovidCases = useSelector(allBrazilCasesSelector)
-  const loadingAllBrazilCovidCases = useSelector(loadingBrazilAllCases)
+  const selectedStateData = useSelector(brazilCitiesCasesSelector)
+  const loadingAllBrazilCovidCases = useSelector(loadingBrazilCitiesCases)
   const lastUpdated = useSelector(lastUpdatedSelector)
   const date = lastUpdated && formatTimeZoneDate(lastUpdated)
-
-  const selectedStateData = allBrazilCovidCases.filter(
-    entry => entry.state === state.toUpperCase() && entry.isLast && entry.placeType === 'city'
-  )
 
   const summaryData = selectedStateData.reduce(
     (acc, curr) => {
       return {
-        cases: acc.cases + curr.confirmed,
+        cases: acc.cases + curr.cases,
         deaths: acc.deaths + curr.deaths,
       }
     },
@@ -46,19 +45,19 @@ const BrazilStateDashboard = ({ state }) => {
   )
 
   useEffect(() => {
-    dispatch(getAllBrazilCases())
+    dispatch(getBrazilCitiesCases({ state: state.toUpperCase() }))
     dispatch(getBrazilLatUpdated())
-  }, [dispatch])
+  }, [dispatch, state])
 
   return (
     <Grid container spacing={2}>
       <Grid item className={stylesDashboard.titleWrapper}>
-        <Typography className={stylesDashboard.title} variant="h1">
-          {t('common:monitorBrazilState')} - {t(`brazil:${state.toUpperCase()}`)}
+        <Typography className={styles.title} variant="h1">
+          {t(`brazil:${state.toUpperCase()}`)}
         </Typography>
       </Grid>
       <Grid item className={stylesDashboard.fullWidth}>
-        <Link component={RouterLink} to="/" variant="body2" className={stylesDashboard.fullWidth}>
+        <Link component={RouterLink} to="/" variant="body2" className={stylesDashboard.link}>
           <Button
             aria-label="ver detalhes"
             color="primary"
---
+++ src/views/dashboard/brazil-state/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/styles'
+
+export default makeStyles(() => ({
+  title: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 600,
+  },
+}))
